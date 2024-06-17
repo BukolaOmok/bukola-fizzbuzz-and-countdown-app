7f91a7488f984e717a765bb8071ec42f32dbefed
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-17 17:48:06 +0100

    add countdown section to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import FizzBuzz from "./FizzBuzz/FizzBuzz.jsx";
-// import Countdown from './Countdown/Countdown.jsx'
 import "./App.css";
+import Countdown from "./Countdown/Countdown.jsx";
 
 export default function App() {
   return (
-    <FizzBuzz />
-  )
+    <div>
+      <h3>FizzBuzz Section</h3>
+      <FizzBuzz />
+      <h3>Countdown Section</h3>
+      <Countdown />
+      <Countdown />
+      <Countdown />
+    </div>
+  );
 }
-
--- src/Countdown/Countdown.jsx
+++ src/Countdown/Countdown.jsx
@@ -1,10 +1,43 @@
 import "../App.css";
+import React from "react";
 
-export default function Countdown(countdownNumber) {
-  if (countdownNumber === 0) {
+export default function Countdown() {
+  const generateRandomCountdownNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  let countdownNumber = generateRandomCountdownNumber(5, 15);
+  const [presentNumber, setPresentNumber] = React.useState(countdownNumber);
+
+  function decrement() {
+    setPresentNumber(presentNumber - 1);
+  }
+
+  function reset() {
+    setPresentNumber(countdownNumber);
+  }
+
+  return (
+    <div className="countdown-update">
+      <button className="countdown--update--decrement" onClick={decrement}>
+        Decrement
+      </button>
+      <div className="countdown--update--content">
+        <h1>{CountdownBreakdown(presentNumber)}</h1>
+      </div>
+      <button className="count--down--update--reset" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+const CountdownBreakdown = (countdownNumber) => {
+  if (countdownNumber === 1) {
     return "👩🏽‍🚀";
-  } else if (countdownNumber === 1) {
+  } else if (countdownNumber <= 0) {
+    countdownNumber = 0;
     return "🚀";
   }
   return countdownNumber;
-}
+};
